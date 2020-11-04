d8cff8d6894ca5c15a5bff02d407e7dfeb37fd2c
nhatnt <nhatnt@vng.com.vn>
2020-11-04 14:09:54 +0700

    chap1:  - ground of being - everything is an object

---
+++ Chap1-TheArchitechtureOfSwift.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+class Dog { func bark() { print("woffff") }}
+
+//In Swift, Everything is an object
+
+//“The idea of everything being an object is a way of suggesting
+//that even “primitive” linguistic entities can be sent messages. ”
+
+extension Int {
+    func sayHello() {
+        print("Hello, I'm \(self)")
+    }
+}
+
+1.sayHello()
+
+//Three flavors of Object Type
+
+
